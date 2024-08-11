e68eedd8c7a2033d2c4217efc4f024b517333593
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-08-11 10:39:21 +0500

    the logic completed succesfully

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-var sample = ["bat", "tap", "cat","tab", "pat"];
+var sample = ["bat", "tap", "cat","tab", "pat",];
 var star = "";
 var store = "";
+var count = 0;
+var output = [];
 for(var i = 0; i < sample.length; i++ ){
     star += sample.splice(i,1)
-    // console.log("first",sample , star, sample.indexOf(star),i)
     star = star.split('')
+    // console.log("first",sample , star, sample.indexOf(star),i)
     for(var j = 0; j < sample.length; j++ ){
             store = sample[j]
             store = store.split('')
-
-console.log(star,store)
+// main function
+for(var k = 0; k < star.length; k++){
+      for(var l = 0; l < store.length; l++){
+            // console.log(star[k],k,store[l],l,counting)
+           if(star[k] == store[l]){
+                 count++
+           } 
+      }
+}
+if(count == star.length){
+      output.push([star.join(""),store.join("")])
+// console.log(true,star,store,count)
+}
+count = 0;
+// console.log(star,i,store,j)
 store = store.join('')
 }
 star = star.join('')
 sample.splice(i,0,star)
 star = ""
 // console.log("second",sample)
-        }
-        
-       
-    
-// rough work:
-
-// var text = ["app"]
-// var text = ["bat", "tap", "cat","tab", "pat"];
-// for(var i = 0 ; i < text.length; i++){
-//     console.log(text.slice(1,2))
-// }
-// // console.log(text);
-
-// // 0,1 \ 1,2
-
-// sbse pehle hmmein do values nikaal ni hongi take hm aikse dusri mattch krwaskeihn thhrough nested loop
-// yhan do value arhi hain aik star me aur dusri store me
-// star me aik aik krke specific values ko turn by turn nikaal kr unhe store krawaya jarha hai through !! sample.splice(i,1) !!
-// store me aik aik krke specific values ko turn by turn nikaal kr unhe store krawaya jarha hai through !! sample[j] !!
-// phr split ki mdd se un specific values ko aik array me convert kya jarha hai take hm dono ko through nested loop chechhk krwaskein
-
-
- 
+}
+console.log(output)
